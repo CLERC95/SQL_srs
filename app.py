b430fc24ca76aa872b6e4aefe3cf35d036c67707
Joel Clerc <j-clerc@ikmail.com>
2025-05-17 15:12:55 +0200

    Select box Added

--- app.py
+++ app.py
@@ -6,5 +6,22 @@ data = {"a": [1, 2, 3], "b": [4, 5, 6]}
 df = pandas.DataFrame(data)
 sql_query = st.text_area(label="Entrez votre input")
 result = duckdb.query(sql_query).df()
+
+
+st.write(
+    """
+#SQL SRS
+Spaced Repetition System SQL practice 
+"""
+)
+
+option = st.selectbox(
+    "What would you like to review ?",
+    ["Joins", "GoupBy", "Windows Functions"],
+    index=None,
+    placeholder="Select a theme",
+)
+st.write("You selected: ", option)
+
 st.write(f"Vous avez entrez la requête suivante: {sql_query}")
 st.dataframe(result)
