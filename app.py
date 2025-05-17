2b839cd2285fca1173873c2e5af32594b486a552
Joel Clerc <j-clerc@ikmail.com>
2025-05-17 15:57:43 +0200

    Correction

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import streamlit as st
-import pandas as pandas
+import pandas as pd
 import duckdb
 
 data = {"a": [1, 2, 3], "b": [4, 5, 6]}
-df = pandas.DataFrame(data)
+df = pd.DataFrame(data)
 sql_query = st.text_area(label="Entrez votre input")
 result = duckdb.query(sql_query).df()
 
