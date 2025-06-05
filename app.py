894d602384e795efe411ffa7f3069d44426708fa
Joel Clerc <j-clerc@ikmail.com>
2025-06-05 17:19:09 +0200

    Ajout d'une database avec les exercices eet un memory state

--- app.py
+++ app.py
@@ -1,77 +1,50 @@
 # pylint: disable=(missing-module-docstring)
-import io
-
 import duckdb
-import pandas as pd
 import streamlit as st
 
-CSV = """
-beverage,price
-orange juice,2.5
-Expresso,2
-Tea,3
-"""
-beverages = pd.read_csv(io.StringIO(CSV))
 
-CSV2 = """
-food_item,food_price
-cookie ,2.5
-chocolatine,2
-muffin,3
-"""
-food_items = pd.read_csv(io.StringIO(CSV2))
-
-ANSWER = """
-SELECT * FROM beverages
-CROSS JOIN food_items
-"""
-solution_df = duckdb.sql(ANSWER).df()
-
-st.header("Entre votre code:")
-
-st.write(
-    """
-# SQL SRS
-## Spaced Repetition System SQL practice 
-"""
-)
+con = duckdb.connect(database="data/exercices_sql_tables.duckdb", read_only=False)
 
 with st.sidebar:
-    option = st.selectbox(
+    theme = st.selectbox(
         "What would you like to review ?",
-        ["Joins", "GoupBy", "Windows Functions"],
+        ["cross_join", "GoupBy", "Windows Functions"],
         index=None,
         placeholder="Select a theme",
     )
-    st.write("You selected: ", option)
+    st.write("You selected: ", theme)
 
+    exercice = con.execute(f"SELECT * FROM memory_state WHERE theme = '{theme}'").df()
+    st.write(exercice)
+
+st.header("Entre votre code:")
 
 sql_query = st.text_area(label="Entrez votre input", key="user_input")
-if sql_query:
-    result = duckdb.sql(sql_query).df()
-    st.dataframe(result)
-    # st.write(f"Vous avez entrez la requête suivante: {sql_query}")
+# if sql_query:
+#     result = duckdb.sql(sql_query).df()
+#     st.dataframe(result)
+#     # st.write(f"Vous avez entrez la requête suivante: {sql_query}")
 
-try:
-    result = result[solution_df.columns]
-    st.dataframe(result.compare(solution_df))
-except KeyError as e:
-    st.write("Some columns are missing")
+# try:
+#     result = result[solution_df.columns]
+#     st.dataframe(result.compare(solution_df))
+# except KeyError as e:
+#     st.write("Some columns are missing")
 
-n_lignes_diff = abs(result.shape[0] - solution_df.shape[0])
-if n_lignes_diff != 0:
-    st.write(f"Result has a {n_lignes_diff} lines difference with the solution")
+# n_lignes_diff = abs(result.shape[0] - solution_df.shape[0])
+# if n_lignes_diff != 0:
+#     st.write(f"Result has a {n_lignes_diff} lines difference with the solution")
 
 
-tab1, tab2 = st.tabs(["Tables", "solution"])
+# tab1, tab2 = st.tabs(["Tables", "solution"])
 
-with tab1:
-    st.write("Table: beverages")
-    st.dataframe(beverages)
-    st.write("Table: food_items")
-    st.dataframe(food_items)
-    st.write("Table attendue:")
-    st.dataframe(solution_df)
+# with tab1:
+#     st.write("Table: beverages")
+#     st.dataframe(beverages)
+#     st.write("Table: food_items")
+#     st.dataframe(food_items)
+#     st.write("Table attendue:")
+#     st.dataframe(solution_df)
 
-with tab2:
-    st.write(ANSWER)
+# with tab2:
+#     st.write(ANSWER)
